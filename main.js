e2c935f5d8e0a4279941d0b6f84c27d382a18ca0
arjuns81 <95590509+arjuns81@users.noreply.github.com>
2022-08-11 17:00:15 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	
+	
+}
+
+function playSound(){
+	
+}
+
+{
+border-width10px;
+background-colorwhite;
+border-styleridge;
+border-colorblue;
+}
+
+{	
+	function new_image()
+	
+		fabric.Image.fromULR('BirthdayImage.jpg'),function.('Img');
+        Block-image-Object.Scaletowidth(700);
+        Block-Image-Object.ScaletoHeight(510);
+      Block-Image-Object.set({
+      });
+top:0
+left:0
+        
+canvas:add(block-image-object);
+
+    }
+     function playsound () {  
+        x.play();
+     }
